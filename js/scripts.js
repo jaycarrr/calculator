303f8776aa3e43ade258d3fbe0b9553d4090b808
Jay Carr <jaycarr@me.com>
2021-01-23 15:46:33 -0800

    initial setup of javascript and index.html; added calculation code from previous lesson

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const num1 = parseInt(prompt("enter a number"));
+const num2 = parseInt(prompt("enter another number"));
+const result = add(num1,num2);
+alert(result)
